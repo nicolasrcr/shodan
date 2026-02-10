b2ed1ec433df6f443a5062bfcecedacda643301e
nicolasrcr <45941286+nicolasrcr@users.noreply.github.com>
2026-02-10 11:20:12 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
         <AuthProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
               <Route path="/" element={<LandingPage />} />
               <Route path="/login" element={<AuthPage />} />
